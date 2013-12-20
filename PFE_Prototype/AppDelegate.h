cb3d59888f2e5ada951908d9d06296a07b1da222
Karim <karim.aboul.enein@gmail.com>
2013-12-20 16:00:11 +0100

    Initial Commit

---
+++ PFE_Prototype/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  PFE_Prototype
+//
+//  Created by Karim Le Nir Aboul-Enein on 20/12/2013.
+//  Copyright Karim Le Nir Aboul-Enein 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ PFE_Prototype/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PFE_Prototype
+//
+//  Created by Karim Le Nir Aboul-Enein on 20/12/2013.
+//  Copyright Karim Le Nir Aboul-Enein 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
