567449c7021cfd0415a3e7e1a8dcae3c0f692f7f
Thibault Varacca <thibaultvaracca@MacBook-Pro-de-Thibault.local>
2014-01-16 17:35:34 +0100

    correction .m -> .mm et réajout de la ressource god

---
+++ PFE_Prototype/ConstructionTowerLayer.h
@@ -0,0 +1,18 @@
+//
+//  ConstructionTowerLayer.h
+//  PFE_Prototype
+//
+//  Created by Thibault Varacca on 16/01/2014.
+//  Copyright 2014 Karim Le Nir Aboul-Enein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "TowerLayer.h"
+#import "TowerData.h"
+
+@interface ConstructionTowerLayer : TowerLayer {
+    
+}
+
+@end
