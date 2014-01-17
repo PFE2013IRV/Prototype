8d74c2745ce44b6da4ef834a7a9ee26d460208ee
Karim <karim.aboul.enein@gmail.com>
2014-01-17 16:05:47 +0100

    Ajout de fichiers ressources pour animations : FireGod_static2, FireGod_static3 et FireGod_colere1 Chargement des fichiers ressources dans le init ElementGodsLayer

--- ElementGodsLayer.mm
+++ ElementGodsLayer.mm
@@ -23,31 +23,70 @@
         // On charge les plists relatives aux différentes spritesheets
         
         [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"FireGod_static1.plist"];
+        [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"FireGod_static2.plist"];
+        [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"FireGod_static3.plist"];
+        [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"FireGod_colere1.plist"];
         
         // On charge les spritesheets en elles-mêmes et on les conserve dans un dictionnaire.
         
         _aFireGodSpriteSheets = [[NSMutableDictionary alloc] init];
         
         CCSpriteBatchNode* pFireGod_static1 = [CCSpriteBatchNode batchNodeWithFile:@"FireGod_static1.png"];
+        CCSpriteBatchNode* pFireGod_static2 = [CCSpriteBatchNode batchNodeWithFile:@"FireGod_static2.png"];
+        CCSpriteBatchNode* pFireGod_static3 = [CCSpriteBatchNode batchNodeWithFile:@"FireGod_static3.png"];
+        CCSpriteBatchNode* pFireGod_colere1 = [CCSpriteBatchNode batchNodeWithFile:@"FireGod_colere1.png"];
         
         [_aFireGodSpriteSheets setObject:pFireGod_static1 forKey:@"FireGod_static1"];
+        [_aFireGodSpriteSheets setObject:pFireGod_static2 forKey:@"FireGod_static2"];
+        [_aFireGodSpriteSheets setObject:pFireGod_static3 forKey:@"FireGod_static3"];
+        [_aFireGodSpriteSheets setObject:pFireGod_colere1 forKey:@"FireGod_colere1"];
         
         [self addChild:[_aFireGodSpriteSheets objectForKey:@"FireGod_static1"]];
+        [self addChild:[_aFireGodSpriteSheets objectForKey:@"FireGod_static2"]];
+        [self addChild:[_aFireGodSpriteSheets objectForKey:@"FireGod_static3"]];
+        [self addChild:[_aFireGodSpriteSheets objectForKey:@"FireGod_colere1"]];
         
+        ///////////////////////
         // Pour chaque spritesheet, on découpe en frames que l'on va conserver dans des tableaux tampons, le temps de l'initialisation.
         
+        // Spritesheets à 4 frames
         NSMutableArray* aFramesFireGod_static1 = [NSMutableArray array];
+        NSMutableArray* aFramesFireGod_static2 = [NSMutableArray array];
+        NSMutableArray* aFramesFireGod_static3 = [NSMutableArray array];
         for (int i=1; i<=4; i++)
         {
             [aFramesFireGod_static1 addObject:
              [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:
-              [NSString stringWithFormat:@"%d.png",i]]];
+              [NSString stringWithFormat:@"static1_%d.png",i]]];
+            
+            [aFramesFireGod_static2 addObject:
+             [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:
+              [NSString stringWithFormat:@"static2_%d.png",i]]];
+            
+            [aFramesFireGod_static3 addObject:
+             [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:
+              [NSString stringWithFormat:@"static3_%d.png",i]]];
+        }
+        
+        // Spritesheets à 16 frames
+        NSMutableArray* aFramesFireGod_colere1 = [NSMutableArray array];
+        for (int i=1; i<=16; i++)
+        {
+            [aFramesFireGod_colere1 addObject:
+             [[CCSpriteFrameCache sharedSpriteFrameCache] spriteFrameByName:
+              [NSString stringWithFormat:@"colere1_%d.png",i]]];
         }
         
         // On initialise pour chaque spritesheet une animation
         
         CCAnimation* pGodFire_static1_Anim = [CCAnimation
                                               animationWithSpriteFrames:aFramesFireGod_static1 delay:0.1f];
+        CCAnimation* pGodFire_static2_Anim = [CCAnimation
+                                              animationWithSpriteFrames:aFramesFireGod_static2 delay:0.1f];
+        CCAnimation* pGodFire_static3_Anim = [CCAnimation
+                                              animationWithSpriteFrames:aFramesFireGod_static3 delay:0.1f];
+        CCAnimation* pGodFire_colere1_Anim = [CCAnimation
+                                              animationWithSpriteFrames:aFramesFireGod_colere1 delay:0.2f];
         
         // On initialise les différentes actions du Dieu du feu et on les conserve dans un dictionnaire
         
@@ -55,9 +94,18 @@
         
         CCAction* pActionGodFire_static1 = [CCRepeatForever actionWithAction:
                                             [CCAnimate actionWithAnimation:pGodFire_static1_Anim]];
+        CCAction* pActionGodFire_static2 = [CCRepeatForever actionWithAction:
+                                            [CCAnimate actionWithAnimation:pGodFire_static2_Anim]];
+        CCAction* pActionGodFire_static3 = [CCRepeatForever actionWithAction:
+                                            [CCAnimate actionWithAnimation:pGodFire_static3_Anim]];
+        CCAction* pActionGodFire_colere1 = [CCRepeatForever actionWithAction:
+                                            [CCAnimate actionWithAnimation:pGodFire_colere1_Anim]];
         
         
         [_aFireGodActions setObject:pActionGodFire_static1 forKey:@"FireGod_static1"];
+        [_aFireGodActions setObject:pActionGodFire_static2 forKey:@"FireGod_static2"];
+        [_aFireGodActions setObject:pActionGodFire_static3 forKey:@"FireGod_static3"];
+        [_aFireGodActions setObject:pActionGodFire_colere1 forKey:@"FireGod_colere1"];
         
         
         ///////////////////////////////////////////////////////////////////
@@ -65,10 +113,10 @@
         ///////////////////////////////////////////////////////////////////
         
         // On lance l'action par défaut : FireGod_static1
-        _pFireGod = [CCSprite spriteWithSpriteFrameName:@"1.png"];
+        _pFireGod = [CCSprite spriteWithSpriteFrameName:@"static3_1.png"];
         
-        [[_aFireGodSpriteSheets objectForKey:@"FireGod_static1"] addChild:_pFireGod];
-        [_pFireGod runAction:[_aFireGodActions objectForKey:@"FireGod_static1"]];
+        [[_aFireGodSpriteSheets objectForKey:@"FireGod_static3"] addChild:_pFireGod];
+        [_pFireGod runAction:[_aFireGodActions objectForKey:@"FireGod_static3"]];
         
         // Positionnement par défaut
         _pFireGod.position = ccp(110, 736);
