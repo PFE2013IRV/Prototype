505e01c1db2404afeae41f6e300e218da6fe43ed
Yann Thebault <MacBook@Macintosh.local>
2014-01-23 15:07:25 +0100

    Mise en place de la physique avec texture associée

--- PFE_Prototype/BalanceTowerLayer.mm
+++ PFE_Prototype/BalanceTowerLayer.mm
@@ -59,9 +59,8 @@ enum {
 		[label setColor:ccc3(0,0,255)];
 		label.position = ccp( s.width/2, s.height-50);
 		
-		[self scheduleUpdate];
+		//[self scheduleUpdate];
 	}
-    
     return self;
 }
 
@@ -71,13 +70,11 @@ enum {
      CGSize s = [[CCDirector sharedDirector] winSize];
      
      b2Vec2 gravity;
-     gravity.Set(0.0f, -1.0f);
+     gravity.Set(0.0f, -0.10f);
      world = new b2World(gravity);
-     
      
      // Do we want to let bodies sleep?
      world->SetAllowSleeping(true);
-     
      world->SetContinuousPhysics(true);
      
      m_debugDraw = new GLESDebugDraw( PTM_RATIO );
@@ -123,7 +120,7 @@ enum {
 }
 
 
-
+/*
 -(void)drawAllBlocsOfTower
 {
     int x = 400;
@@ -145,7 +142,89 @@ enum {
         [self addChild:pBlocSprite];
     }
 }
+  */
 
+
+-(void)drawAllBlocsOfTower
+{
+    int x = 400;
+    int y = 200;
+    
+    for (BlocData *bloc in self._pTowerData._aBlocs)
+    {
+        CCSprite *pBlocSprite = [BlocManager GetSpriteFromModel:bloc];
+        
+        y += bloc._scaledSize.height / 2;
+        
+        int gravityCenterOfBloc = bloc._scaledSize.width / 2 - bloc._gravityCenter.x;
+        
+        pBlocSprite.position = CGPointMake(x + gravityCenterOfBloc, y);
+        [self._aBlocsTowerSprite addObject:pBlocSprite];
+        [self addChild:pBlocSprite];
+        
+        [self createBodyForSprite:pBlocSprite BlocData:bloc Point:CGPointMake(x + gravityCenterOfBloc, y)];
+        
+        y += bloc._scaledSize.height / 2;
+    }
+}
+
+
+
+
+-(void)createBodyForSprite:(CCSprite*)sprite BlocData:(BlocData*)b Point:(CGPoint)p
+{
+    std::vector<CGPoint> forme = [b GetPhysicalVertices];
+    CGPoint pointTmp;
+ 
+    CCLOG(@"Add sprite %0.2f x %02.f",p.x,p.y);
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(p.x/PTM_RATIO, p.y/PTM_RATIO);
+    bodyDef.userData = sprite;
+    b2Body *body = world->CreateBody(&bodyDef);
+    
+    b2Vec2 vertices[100];
+ 
+    CCLOG(@"Call Addbloc");
+    for(int i = forme.size() ; i >= 0  ; i--)
+    {
+        pointTmp = forme[i];
+        vertices[i].Set( (double) pointTmp.y/PTM_RATIO,(double) pointTmp.x/PTM_RATIO);
+        CCLOG(@"Add sprite %0.f x %0.f",vertices[i].x,vertices[i].y);
+    }
+    
+    b2PolygonShape polygonShape;
+    polygonShape.Set(vertices, forme.size()); //pass array to the shape
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &polygonShape;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+    body->CreateFixture(&fixtureDef);/*    CCNode *parent = [self getChildByTag:kTagParentNode];
+    int idx = (CCRANDOM_0_1() > .5 ? 0:1);
+    int idy = (CCRANDOM_0_1() > .5 ? 0:1);
+    CCPhysicsSprite *sprite = [CCPhysicsSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(32 * idx,32 * idy,32,32)];
+    
+    [parent addChild:sprite];
+    [sprite setPTMRatio:PTM_RATIO];
+    [sprite setB2Body:body];
+    [sprite setPosition: ccp( p.x, p.y)];*/
+}
+    
+-(void) update: (ccTime) dt
+{
+	//It is recommended that a fixed time step is used with Box2D for stability
+	//of the simulation, however, we are using a variable time step here.
+	//You need to make an informed choice, the following URL is useful
+	//http://gafferongames.com/game-physics/fix-your-timestep/
+	
+	int32 velocityIterations = 8;
+	int32 positionIterations = 1;
+	
+	// Instruct the world to perform a single step of simulation. It is
+	// generally best to keep the time step and iterations fixed.
+	world->Step(dt, velocityIterations, positionIterations);
+}
 
 -(void) draw
 {
@@ -209,7 +288,7 @@ enum {
     
     CGPoint pointTmp;
     
-
+    
     CCLOG(@"Add sprite %0.2f x %02.f",p.x,p.y);
     
     // Define the dynamic body.
@@ -238,7 +317,7 @@ enum {
     
     b2Vec2 vertices[100];
     
-
+    
     
     
     CCLOG(@"Call Addbloc");
@@ -279,20 +358,20 @@ enum {
         
     }
     
-/*
-    
-    b2Vec2 vertices[5];
-    
-    vertices[0].Set(-1,  2);
-    
-    vertices[1].Set(-1,  0);
-    
-    vertices[2].Set( 0, -3);
-    
-    vertices[3].Set( 1,  0);
-    
-    vertices[4].Set( 1,  1);
- */
+    /*
+     
+     b2Vec2 vertices[5];
+     
+     vertices[0].Set(-1,  2);
+     
+     vertices[1].Set(-1,  0);
+     
+     vertices[2].Set( 0, -3);
+     
+     vertices[3].Set( 1,  0);
+     
+     vertices[4].Set( 1,  1);
+     */
     
     b2PolygonShape polygonShape;
     
@@ -339,20 +418,5 @@ enum {
     
     
 }
--(void) update: (ccTime) dt
-{
-	//It is recommended that a fixed time step is used with Box2D for stability
-	//of the simulation, however, we are using a variable time step here.
-	//You need to make an informed choice, the following URL is useful
-	//http://gafferongames.com/game-physics/fix-your-timestep/
-	
-	int32 velocityIterations = 8;
-	int32 positionIterations = 1;
-	
-	// Instruct the world to perform a single step of simulation. It is
-	// generally best to keep the time step and iterations fixed.
-	world->Step(dt, velocityIterations, positionIterations);
-}
-
 
 @end
